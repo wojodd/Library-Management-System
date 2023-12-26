89f9f0e4737f65cfd419652d1ce7c26ba77587c7
Ameerah Albekhit <istellar.dev02@outlook.com>
2023-12-26 14:45:47 +0300

    secand commit

--- Library_MS/users/admin.py
+++ Library_MS/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import *
+
+admin.site.register(CustomUsers)
+
+admin.site.register(gender)
+
+admin.site.register(Libraries)
+
+admin.site.register(Despositves)
+
+admin.site.register(Semester)
+
+admin.site.register(Cities)
+admin.site.register(Rolel)
